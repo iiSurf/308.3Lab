e9c4bd608600fb525d6a07a17e3fe8ac253ef97f
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-07 15:19:24 -0700

    fixing error from git communicating with VS Code

---
+++ script.mjs
@@ -0,0 +1,12 @@
+// Use only for loops, for of loops, or while loops to implement the solution.
+for (let num = 1; num < 101; num++) {
+    if (num % 3 == 0) {
+        console.log(`Fizz`);
+    } else if (num % 5 == 0) {
+        console.log(`Buzz`);
+    } else if (num % 3 == 0 && num % 5 == 0) {
+        console.log(`Fizz Buzz`);
+    } else if (!(num % 3 == 0 && num % 5 == 0)) {
+        console.log(num);
+    }
+}
