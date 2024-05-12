854bd80c3546b8b7a27c1c0b909567ad700fe18e
iiSurf <87113036+iiSurf@users.noreply.github.com>
2024-05-11 22:53:39 -0700

    try again

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
-// Part 1
+// TODO: Part 1 ----------------------------------------------------------------
 
 // To begin, solve the following classic “Fizz Buzz” problem. There are a few different ways to do this - experiment with what you think is the most efficient. Once you have solved the problem, ask yourself if there could be another way; and if so, would it be better.
 // Accomplish the following:
@@ -22,7 +22,7 @@ for (let num = 1; num < 101; num++) {
         console.log(num);
     }
 }
-// Part 2
+// TODO: Part 2 ---------------------------------------------------------------
 // Now we will move onto something slightly more complex.
 // Context: A prime number is any whole number greater than 1 that cannot be exactly divided by any whole number other than itself and 1. For example, the number 5 is prime because it cannot be divided by 4, 3, or 2; it can only be divided by itself (5) and 1. Similarly, the numbers 7 and 11 are prime. As numbers become larger, determining whether or not they are prime is increasingly difficult, but loops make this process relatively easy!
 // TODO: Write a script that accomplishes the following: 
@@ -34,16 +34,218 @@ for (let num = 1; num < 101; num++) {
 
 // find the first prime number all the way to 100;
 // process of elimination. divide by itself and one and positive.
-let n = 50;
-for (let num2 = n; num2 < 100;num2++) {
-    let prime = true;
-    for (let notPrimeNum = 2; notPrimeNum < num2; notPrimeNum++) {
-    if (notrimeNum is < 2) {
-        primeNum = false;
-        } else {
-            for (let isPrimeNum = 2; isPrimeNum  ) {
-                if 
-                }
-            }
+// let n = 2; // n is an arbitrary number
+// let counterPart2 = 0;
+// We start with the given arbitrary number n.
+// The outer for loop iterates from n + 1 onwards until we find a prime number.
+// The inner for loop checks if the current number is divisible by any number from 2 up to its square root. If it is, we set isDivisible to true.
+// If the inner loop completes without finding any divisors, we log the current number as the next prime number and exit the outer loop.
+
+let n = 11;
+
+let isPrime = false;
+for (let i = n + 1; !isPrime; i++) {
+  let isDivisible = false;
+  for (let j = 2; j <= Math.sqrt(i); j++) {
+    if (i % j === 0) {
+      isDivisible = true;
+      break;
     }
+  }
+  if (!isDivisible) {
+    console.log(`The next prime number after ${n} is going to be ${i}`);
+    isPrime = true;
+  }
+}
+
+// TODO: Part 3 -----------------------------------------------------
+
+// Assumptions
+// 4 cells per row and no escape characters other than \n
+
+// What variables do I need to create to be able to make logical code
+
+// if (char === "\n") console.log(`We found a new line`);
+
+// My attempt to follow along wth Dylan
+
+// const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+
+// My attempt at following Dylans code in class and was unsuccessful. His code is down below.
+// let cell1 = ``;
+// let cell2 = ``;
+// let cell3 = ``;
+// let cell4 = ``;
+// let placeholder = ``;
+// let counter = 0;
+
+// for (let char of str) {
+//     switch (char) {
+//     // If comma do this
+//         case ",":
+//             counter++;
+//             if (!cell1) {
+//                 cell1 = placeholder;
+//                 placeholder = ``;
+//             } else if (!cell2) {
+//                 cell2 = placeholder;
+//                 placeholder = ``;
+//                }
+//                else if (!cell3) {
+//                 cell3 = placeholder;
+//                 placeholder = ``;
+//                }
+//                else if (!cell4) {
+//                 cell4 = placeholder;
+//                 placeholder = ``;
+//                }
+//                 let cell1 = ``;
+//                 let cell2 = ``;
+//                  let cell4 = ``;
+//                 let cell3 = ``;
+//             }
+//             // console.log(`Found comma`);
+//                 break;
+//         // If new line do this
+//                 case "\n":
+//                    counter++;
+//                   cell4 = placeholder;
+//                   console.log(cell1, cell2, cell3, cell4);
+//             // console.log(`Found New LIne`);
+//                 break;
+//         // If char do this
+//                 default:
+//                 counter++;
+//                 placeholder += char;
+//             // console.log(`Found char`);
+//                 // console.log(`Found placeholder`);
+//                 if(counter === str.length) {
+//                     cell4 = placeholder;
+//                     placeholder = ``;
+//                     console.log(cell1, cell2, cell3, cell4);
+//                     cell1 = ``;
+//                     cell2 = ``;
+//                     cell3 = ``;
+//                     cell4 = ``;
+//                 } else {
+//                     counter++;
+//                     placeholder
+//                 }
+//                 break;
+//     }
+// }
+
+
+// DYLANS CODE:
+
+// What are we accomplishing:
+//Loop through the characters of a given CSV string.
+// Store each "cell" of data in a variable.
+// When you encounter a comma, move to the next cell.
+// When you encounter the "\r\n" sequence, move to the next "row."
+// Log each row of data.
+// You do not need to format the data, the following works well.
+// console.log(cell1, cell2, cell3, cell4);
+
+//Assumptions:
+// There will only be 4 cells per row.
+// There will be no escaped characters other than "\n".
+
+// Variables to hold string and cell data
+// const str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
+
+// for (let char of str) {
+//   switch (char) {
+//     // If comma do this
+//     case ',':
+//       counter++;
+//       if (!cell1) {
+//         cell1 = placeholder;
+//         placeholder = ``;
+//       } else if (!cell2) {
+//         cell2 = placeholder;
+//         placeholder = ``;
+//       } else {
+//         cell3 = placeholder;
+//         placeholder = ``;
+//       }
+//       break;
+//     //If new line do this
+//     case '\n':
+//       counter++;
+//       cell4 = placeholder;
+//       placeholder = ``;
+//       console.log(cell1, cell2, cell3, cell4);
+//       cell1 = ``;
+//       cell2 = ``;
+//       cell3 = ``;
+//       cell4 = ``;
+//       break;
+//     //If char do this
+//     default:
+//       counter++;
+//       placeholder += char;
+
+//       if (counter === str.length) { //If it's our last character we populate cell4 and print
+//         cell4 = placeholder;
+//         placeholder = ``;
+//         console.log(cell1, cell2, cell3, cell4);
+//         cell1 = ``;
+//         cell2 = ``;
+//         cell3 = ``;
+//         cell4 = ``;
+//         counter = 0;
+//       }
+//       break;
+//   }
+// }
+const str = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`;
+
+let cell1 = ``;
+let cell2 = ``;
+let cell3 = ``;
+let cell4 = ``;
+let placeholder = ``;
+let counter = 0;
+
+for (let char of str) {
+  // If comma do this
+  if (char === ' ,') {
+    counter++;
+    if (!cell1) {
+      cell1 = placeholder;
+      placeholder = ``;
+    } else if (!cell2) {
+      cell2 = placeholder;
+      placeholder = ``;
+    } else {
+      cell3 = placeholder;
+      placeholder = ``;
+    }
+  } else if (char === ' \n') {
+    //If new line do this
+    counter++;
+    cell4 = placeholder;
+    placeholder = ``;
+    console.log(cell1, cell2, cell3, cell4);
+    cell1 = ``;
+    cell2 = ``;
+    cell3 = ``;
+    cell4 = ``;
+  } else {
+    counter++;
+    placeholder += char;
+
+    if (counter === str.length) {
+      //If it's our last character we populate cell4 and print
+      cell4 = placeholder;
+      placeholder = ``;
+      console.log(cell1, cell2, cell3, cell4);
+      cell1 = ``;
+      cell2 = ``;
+      cell3 = ``;
+      cell4 = ``;
+      counter = 0;
+    }
+  }
 }
